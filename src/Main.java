1427a96b20e6fad5b210aab17ea2be733480a3ec
Zelendewagen <Zelendewagen@gmail.com>
2023-04-20 20:44:31 +1000

    tree

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,112 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        TestList lst = new TestList();
+        Tree node = new Tree();
+        Scanner scan = new Scanner(System.in);
 
-        lst.addFirst(1);
-        lst.addFirst(2);
-        lst.addFirst(3);
-        lst.addFirst(4);
-        lst.addFirst(5);
-
-
-        lst.show();
-        lst.reverse();
-        System.out.println("reverse");
-        lst.show();
-
+        char ch;
+        do {
+            System.out.println("Введите число");
+            int num = scan.nextInt();
+            node.root = node.insert(node.root, num);
+            node.inorder(node.root);
+            System.out.println("\nПродолжить?(Y, N)");
+            ch = scan.next().charAt(0);
+        } while (ch == 'Y' || ch == 'y');
     }
 }
 
-class TestList {
-    Node head;
 
-    public void show() {
-        Node temp = head;
+class Tree {
 
-        while (temp != null) {
-            System.out.println(temp.value);
-            temp = temp.getNext();
+    Node root = null;
+
+    Node rotateLeft(Node myNode) {
+        System.out.print("left\n");
+        Node child = myNode.right;
+        Node childLeft = child.left;
+
+        child.left = myNode;
+        myNode.right = childLeft;
+
+        return child;
+    }
+
+    Node rotateRight(Node myNode) {
+        System.out.print("right\n");
+        Node child = myNode.left;
+        Node childRight = child.right;
+
+        child.right = myNode;
+        myNode.left = childRight;
+
+        return child;
+    }
+
+    boolean isRed(Node myNode) {
+        if (myNode == null) {
+            return false;
+        }
+        return (myNode.color == true);
+    }
+
+    void swapColors(Node node1, Node node2) {
+        boolean temp = node1.color;
+        node1.color = node2.color;
+        node2.color = temp;
+    }
+
+    Node insert(Node myNode, int data) {
+        if (myNode == null) {
+            return new Node(data);
+        }
+        if (data < myNode.value) {
+            myNode.left = insert(myNode.left, data);
+        } else if (data > myNode.value) {
+            myNode.right = insert(myNode.right, data);
+        } else {
+            return myNode;
+        }
+        if (isRed(myNode.right) && !isRed(myNode.left)) {
+            myNode = rotateLeft(myNode);
+            swapColors(myNode, myNode.left);
+        }
+        if (isRed(myNode.left) && isRed(myNode.left.left)) {
+            myNode = rotateRight(myNode);
+            swapColors(myNode, myNode.right);
+        }
+        if (isRed(myNode.left) && isRed(myNode.right)) {
+            myNode.color = !myNode.color;
+            myNode.left.color = false;
+            myNode.right.color = false;
+        }
+        return myNode;
+    }
+
+    void inorder(Node node) {
+        if (node != null) {
+            inorder(node.left);
+            char c = '●';
+            if (node.color == false)
+                c = '◯';
+            System.out.print(node.value + "" + c + " ");
+            inorder(node.right);
         }
     }
 
-    public void addFirst(int value) {
-        Node node = new Node(value);
-        node.setNext(head);
-        head = node;
-    }
+}
 
-    public void reverse() {
-        Node current = head;
-        head = null;
-        while (current.next != null) {
-            this.addFirst(current.value);
-            current = current.next;
-        }
-        this.addFirst(current.value);
-    }
+class Node {
+    Node left, right;
+    int value;
+    boolean color; // красный
 
-
-    static class Node {
-        public int value;
-        public Node next;
-        public Node prev;
-
-        public Node(int value) {
-            this.value = value;
-            this.next = null;
-            this.prev = null;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
-
-
+    Node(int value) {
+        this.value = value;
+        left = null;
+        right = null;
+        color = true;
     }
 }
 
